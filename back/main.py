955a5ba495324e29af28685f485f62cfeeb5b49c
Zzebastian <sebastian.scheverin@gmail.com>
2023-12-09 08:29:49 -0300

    Inicio de Base de datos(BdD)

---
+++ back/main.py
@@ -0,0 +1,47 @@
+﻿import sqlite3
+
+import os
+
+text = os.path.dirname(os.path.relpath(__file__))
+
+
+conexion = sqlite3.connect(f'{text}/ENTRENAMIENTO.sqlite')
+cursor = conexion.cursor()
+text = []
+text.append('''CREATE TABLE IF NOT EXISTS grupoMuscular (
+            idGrupoM PRIMARY KEY,
+            grupoM
+            )''')
+text.append('''CREATE TABLE IF NOT EXISTS entrenamientos (
+            idEntrenamiento INTEGER PRIMARY KEY AUTOINCREMENT,
+            nombreEntrenamiento,
+            Repeticiones,
+            IncrementoPeso,
+            idGrupoM,
+            FOREIGN KEY (idGrupoM) REFERENCES grupoMuscular (idGrupoM)
+            )''')
+text.append('''CREATE TABLE IF NOT EXISTS nombreRutinas (
+            idRutina PRIMARY KEY,
+            nombreRutina
+            )''')
+text.append('''CREATE TABLE IF NOT EXISTS rutinas (
+            idRutina,
+            idEjercicio,
+            PRiMARY KEY (idRutina, idEjercicio)
+            )''')
+text.append('''CREATE TABLE IF NOT EXISTS diario (
+            idDia,
+            idRutina1,
+            idRutina2,
+            FOREIGN KEY (idRutina1) REFERENCES rutinas (idRutina)
+            )''')
+
+for elemento in text:
+    cursor.execute(elemento)
+    print('hecho')
+
+
+
+
+conexion.commit()
+conexion.close()
